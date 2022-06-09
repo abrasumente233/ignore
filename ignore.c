f177a7dc446e67e521d22a2e13a02c7be26c2b35
abrasumente <345396594@qq.com>
2022-06-09 23:23:11 +0800

    list available gitignores

--- ignore.c
+++ ignore.c
@@ -18,12 +18,26 @@ void find_gitignore_entry(const char *ignore_name) {
     printf("%s\n", "Not found");
 }
 
+void list_gitignore_entries() {
+    int i = 0;
+    for (i = 0; i < ignore_table_size; i++) {
+        printf("    %s\n", ignore_table[i].name);
+    }
+}
+
 int main(int argc, const char *argv[]) {
+    if (argc == 1 || strcmp(argv[1], "list") == 0) {
+        printf("Available gitignores:\n\n");
+        list_gitignore_entries();
+        return 0;
+    }
+
     if (argc != 2) {
         printf("Usage: %s <ignore-type>\n", argv[0]);
         return 1;
     }
 
     const char *ignore_name = argv[1];
+
     find_gitignore_entry(ignore_name);
 }
